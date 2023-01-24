0df13e6e6a18f3fcfac7f6be2d92bcb5c9d3276c
Brian Straub <cbstraub@protonmail.com>
2023-01-24 14:49:27 -0700

    frontend changed to client

--- backend/server.js
+++ backend/server.js
@@ -15,9 +15,9 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
-app.use(express.static(path.resolve(__dirname, '../frontend/build')));
+app.use(express.static(path.resolve(__dirname, '../client/build')));
 app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../frontend/build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
   });
 
 
