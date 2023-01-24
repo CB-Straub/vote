3eefcd20170600f9657aeefde7eaea631baa8887
Brian Straub <cbstraub@protonmail.com>
2023-01-24 15:58:19 -0700

    "path" defined for express.static

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const dotenv = require('dotenv').config()
 const PORT = process.env.PORT || 5000
 const {errorHandler} = require('./middleware/errorMiddleware')
 const colors = require('colors')
+const path = require('path') 
 
 
 const connectDB = require('./config/database')
@@ -15,14 +16,16 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
-// app.use(express.static(path.resolve(__dirname, '../frontend/build')));
-// app.get('/*', (req, res) => {
-//     res.sendFile(path.resolve(__dirname, '../frontend/build', 'index.html'));
-//   });
+
 
 
 app.use('/api/votes', require('./routes/voteRoutes'))
 app.use('/api/users', require('./routes/userRoutes'))
+
+app.use(express.static(path.resolve(__dirname, '../frontend/build')));
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../frontend/build', 'index.html'));
+  });
 
 app.use(errorHandler)
 
