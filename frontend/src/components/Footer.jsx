0ce2acee952cb319c7c56943e742952df4574738
Brian Straub <cbstraub@protonmail.com>
2023-01-24 15:13:37 -0700

    footer component added

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='footer-container'>
+        <p>@2022 Vanomoose SSP, LLC</p>
+    </div>
+  )
+}
+
+export default Footer
